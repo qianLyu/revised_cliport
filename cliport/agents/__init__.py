f7a18cb36af9c2ed90c0be6ae1c866299720ba1f
qian <luoqian19961224@gmail.com>
2022-09-08 16:06:02 +0800

    add representations in baselines

--- cliport/agents/__init__.py
+++ cliport/agents/__init__.py
@@ -40,6 +40,7 @@ names = {
 
          # RN50-BERT
          'two_stream_full_rn50_bert_lingunet_lat_transporter': TwoStreamRN50BertLingUNetLatTransporterAgent,
+         'rn50_bert': TwoStreamRN50BertLingUNetLatTransporterAgent,
 
          # RN50-BERT without language
          'two_stream_full_rn50_bert_unet_transporter': TwoStreamRN50BertUNetTransporterAgent,
@@ -49,6 +50,7 @@ names = {
 
          # Untrained RN50-BERT (similar to untrained CLIP)
          'two_stream_full_untrained_rn50_bert_lingunet_transporter': TwoStreamUntrainedRN50BertLingUNetTransporterAgent,
+         'u_rn50_bert': TwoStreamUntrainedRN50BertLingUNetTransporterAgent,
 
          ###################################
          ### Single-Stream Architectures ###
@@ -72,4 +74,5 @@ names = {
 
          # CLIPort with FiLM language fusion
          'two_stream_clip_film_lingunet_lat_transporter': TwoStreamClipFilmLingUNetLatTransporterAgent,
+         'cliport_film': TwoStreamClipFilmLingUNetLatTransporterAgent,
          }
